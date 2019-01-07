30fa995e2f047d1147074aceeba008481793c122
vagish dixit <dixit.vagish@gmail.com>
2019-01-07 13:39:03 +0530

    first commit

---
+++ src/QuestionToAttempt.java
@@ -0,0 +1,20 @@
+public class QuestionToAttempt {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Stack");
+        System.out.println("1.Queue using stack");
+        System.out.println("2.Double Stack");
+        System.out.println("3.Infix to Postfix");
+        System.out.println("4.Prefix to infix");
+        System.out.println("5.Postfix to Prefix");
+        System.out.println("6.Balanced Parentheses");
+        System.out.println("7.Next Greater Element");
+        System.out.println("8.Expression Evaluation");
+        System.out.println("9.Reverse A Stack");
+        System.out.println("10.Delete Middle Element");
+
+
+    }
+}
